d7d80eb77b98d9cbf7b81f42d0ce33910d659a68
GameBoyJak <parrottoalex@gmail.com>
2020-09-17 01:57:14 -0400

    Added Protoype

---
+++ P1/src/main.js
@@ -0,0 +1,59 @@
+let canvas, ctx;
+const canvasWidth = 1200, canvasHeight = 800;
+const cellWidth = 10;
+const fps = 12;
+let lifeworld;
+let color;
+let root;
+
+window.onload = init;
+
+function init(){
+	canvas = document.querySelector("canvas");
+	canvas.width = canvasWidth;
+	canvas.height = canvasHeight;
+	ctx = canvas.getContext("2d");
+	// TODO: init lifeworld
+	lifeworld = new Lifeworld(120,90,.2);
+	color = 'red';
+	root = document.documentElement;
+	loop();
+
+	document.querySelector('#colorChooser').onchange = function(e) {
+		color = e.target.value;
+		root.style.setProperty('--border-color', e.target.value);
+	}
+}
+
+function loop(){
+	setTimeout(loop,1000/fps);
+	// TODO: update lifeworld
+	drawBackground();
+	drawWorld();
+	lifeworld.step();
+}
+
+function drawBackground(){
+	ctx.save();
+	ctx.fillStyle = "black";
+	ctx.globalAlpha = 4/fps;
+	ctx.fillRect(0,0,canvasWidth,canvasHeight);
+	ctx.restore();
+}
+
+function drawWorld(){
+	ctx.save();
+	for(let col = 0; col < lifeworld.numCols; col++) {
+		for(let row = 0; row < lifeworld.numRows; row++){
+			drawCell(col,row,cellWidth, lifeworld.world[col][row]);
+		}
+	}
+	ctx.restore();
+}
+
+function drawCell(col,row,dimensions,alive) {
+	ctx.beginPath();
+	ctx.rect(col*dimensions, row*dimensions, dimensions, dimensions);
+	ctx.fillStyle = alive ? `${color}` : 'rgba(0,0,0,0)';
+	ctx.fill();
+}
